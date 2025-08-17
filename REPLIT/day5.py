61ea9d5edaba08a1227227cec3611bd95029edfe
SumedhShegokar <SumedhShegokar@gmail.com>
2025-08-17 15:19:12 +0530

    Day 5&6 Done

---
+++ REPLIT/day5.py
@@ -0,0 +1,9 @@
+#hey this is a comment
+# This is a simple Python script that prints "Hello, World!".
+"""hi
+This is a multi-line comment.
+It can span multiple lines.
+"""
+print("Hello, World!")
+#day5
+print("hey i am a \"Good boy\" also smart")
